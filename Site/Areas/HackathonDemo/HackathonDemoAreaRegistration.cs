999e05ba12f8373a2324948e16d6712848f95972
Manish Mehmood <m.mehmood@contentbloom.com>
2018-09-02 17:15:44 +0530

    Added Search Module/ DXA Theming Views

---
+++ Site/Areas/HackathonDemo/HackathonDemoAreaRegistration.cs
@@ -0,0 +1,43 @@
+﻿using System.Web.Mvc;
+using Sdl.Web.Common.Models;
+using Sdl.Web.Mvc.Configuration;
+using Sdl.Web.Modules.Core.Models;
+
+namespace Sdl.Web.Site.Areas.HackathonDemo
+{
+    public class HackathonDemoAreaRegistration : BaseAreaRegistration
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "HackathonDemo";
+            }
+        }
+
+        //public override void RegisterArea(AreaRegistrationContext context) 
+        //{
+        //    context.MapRoute(
+        //        "HackathonDemo_default",
+        //        "HackathonDemo/{controller}/{action}/{id}",
+        //        new { action = "Index", id = UrlParameter.Optional }
+        //    );
+        //}
+        protected override void RegisterAllViewModels()
+        {
+            // Entity Views
+      
+
+            // Page Views
+            //RegisterViewModel("IncludePage", typeof(PageModel));
+            RegisterViewModel("Index", typeof(PageModel));
+            RegisterViewModel("HomePage", typeof(Link));
+            // Region Views for Include Pages
+            RegisterViewModel("Header", typeof(Link));
+            RegisterViewModel("Footer", typeof(RegionModel));
+            RegisterViewModel("FooterLinks", typeof(LinkList<Link>));
+            RegisterViewModel("Contact", typeof(LinkList<Link>));
+            
+        }
+    }
+}
